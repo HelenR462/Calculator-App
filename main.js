195e04769827b312ad206b2bac7a69aeb202ecea
HelenR462 <69416811+HelenR462@users.noreply.github.com>
2022-11-15 13:00:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,117 +4,113 @@ const numBtn = document.querySelectorAll(".number");
 const operatorBtn = document.querySelectorAll(".operator");
 
 const clrBtn = document.querySelector("#clear");
-clrBtn.addEventListener("click", (e) => {
+clrBtn.addEventListener("click", () => {
   input.value = "";
 });
 
-//  STEP 1 Press 1
-// EXPECTED= 1 to show on screen
-// ACTUAL= IT WORKS!
-
-// STEP 2 Press +
-// EXPTECED= 1+ to show on screen
+// STEP 1 Press 11
+// EXPECTED = 11  to show on screen
 // ACTUAL= IT WORKS
 
-// STEP 3 Press 2
-// EXPECTED= 2 to show on screen
-// ACTUAL= IT WORKS!
+// STEP 2 Press +
+// EXPECTED = 11+ to show on screen
+// ACTUAL= IT WORKS
+
+// STEP 3 Press 22
+// EXPECTED = 11 + 22 to show on screen
+// ACTUAL= IT WORKS
 
 // STEP 4 Press =
-// EXPECTED = result to show on screen
-// ACTUAL=  Displays empty screen
-
-// STEP 5 Press operator 
-// EXPECTED = not to repeat
-// ACTUAL= except for '+' the rest DO NOT WORK
-
+// EXPECTED = 33 to show on screen
+// ACTUAL= 13
 
 let firstOperand = null; //working
 let operator = null; //working
-let secondOperand = null;  //working
+let secondOperand = null; //working
 let result = null;
 
-
- 
 numBtn.forEach((button) => {
   button.addEventListener("click", (e) => {
-   
-    input.value +=button.textContent ;
-  
-    if(input.value === '.'&& this.secondOperand.includes('.')){
-       input.value =''
+    if (operator == null) {
+      firstOperand = button.textContent;
+      input.value += firstOperand;
+    } else {
+      secondOperand = button.textContent;
+      input.value += secondOperand;
+    }
+    // if first operand is null
+    if (firstOperand == null) {
+      // set the firstoperand equal button text content
+      firstOperand = button.textContent;
+      secondOperand = input.value;
+      // append the button text content to first operand
+    } else {
+      firstOperand = input.value;
+      secondOperand = button.textContent;
     }
 
+    if (secondOperand == null) {
+      secondOperand = button.textContent;
+    }
 
+    console.log("firstOperand: ", firstOperand);
+    console.log("secondOperand: ", secondOperand);
+    console.log("operator: ", operator);
+  });
 
+  operatorBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      //code this =>  set the operator variable to operator symbol
+      // const concat= "";
+      if (operator == null) {
+        operator = button.textContent;
+        input.value += operator;
+      }
+    });
+  });
 });
-});
 
-
-
-operatorBtn.forEach((button) => {
-  button.addEventListener("click", (e) => {
- 
-     input.value += button.textContent;
-  
-     if(input.value === '+'||
-        input.value === '-' ||input.value == '*' ||input.value == '/'
-        && this.operator.includes(
-          ['+','-','*','/']) 
-          ){
-      input.value = ''
-   }
-  
- 
-
-});
-  
 const decimal = document.getElementById("decimal");
-decimal.addEventListener("click", (e) => {
- 
+decimal.addEventListener("click", () => {
   input.value = decimal.textContent;
-     
-     
 });
 
 const equalsBtn = document.getElementById("compute");
 equalsBtn.addEventListener("click", compute);
-  
-function compute
-    (firstOperand, operator, secondOperand) {
-  firstOperand = parseInt(this.firstOperand);
-      secondOperand = parseInt(this.secondOperand);
-  if (isNaN(secondOperand) || isNaN(firstOperand)){
-   input.value = ''
-      switch (this.operator) {
 
-        case "+":
-          input.value = this.firstOperand + this.secondOperand;
-          break;
+function compute() {
+  console.log("firstOperand: ", firstOperand);
+  console.log("secondOperand: ", secondOperand);
+  console.log("operator: ", operator);
 
-        case "-":
-          input.value = firstOperand - secondOperand;
-          break;
+  // let operator = "operator";
+  for (let i = 0; i < operator.length; i++) {
+    operator.textContent += operator[i] + "<br>";
 
-        case "*":
-          input.value = firstOperand * secondOperand;
-          break;
+    switch (operator) {
+      case "+":
+        input.value = parseInt(firstOperand) + parseInt(secondOperand);
+        break;
 
-        case "÷":
-          input.value = firstOperand / secondOperand;
-          break;
+      case "-":
+        input.value = parseInt(firstOperand) - parseInt(secondOperand);
 
-        default:
-          this.firstOperand = this.secondOperand;
-          this.operator = operator;
-          this.secondOperand = '';
-      }
+        break;
+
+      case "*":
+        input.value = firstOperand * secondOperand;
+        // input.value = parseInt(firstOperand) * parseInt(secondOperand);
+        break;
+
+      case "/":
+        input.value = firstOperand / secondOperand;
+        // input.value = parseInt(firstOperand) / parseInt(secondOperand);
+        break;
+
+      default:
+        firstOperand = secondOperand;
+        operator = operator;
+        secondOperand = "";
     }
   }
-})
-
-
-
-
- 
-
+}
